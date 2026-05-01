7213cada0990c751364fac6bc1da99e5da6db4ef
Ashas Vokabyr <sasha.rybakov228@gmail.com>
2026-05-01 23:01:44 +0300

    updating output

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -9,7 +9,7 @@ class UserController {
 
     private function sendResponse($data, $statusCode = 200) {
         http_response_code($statusCode);
-        echo json_encode($data, JSON_UNESCAPED_UNICODE);
+        echo json_encode($data, JSON_UNESCAPED_UNICODE) . "\n";
         exit;
     }
 
